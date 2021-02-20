e8c377b6239d702d6cd8dbca252ee7cc0d77351c
ali <ali0995559052@gmail.com>
2021-02-20 06:00:53 -0800

    add the change

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 const Home = () => {
   return (
@@ -6,7 +6,7 @@ const Home = () => {
       <h1>home page</h1>
       <p>this app is upload in vercel pltform</p>
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
