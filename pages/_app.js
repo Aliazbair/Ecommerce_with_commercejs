ed8ffea813134660503681ab880d1f66ef8cf5e8
ali <ali0995559052@gmail.com>
2021-02-21 13:08:46 -0800

    create Store and reducer

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,25 @@
-import React, { useEffect } from 'react'
+import { StoreProvider } from '../components/Store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import Router from 'next/router'
+
+// steup router with Nprogress
+Router.events.on('routerChangeState', () => NProgress.start())
+Router.events.on('routerChangeComplete', () => NProgress.done())
+Router.events.on('routerChangeError', () => NProgress.done())
 
 export default function MyApp({ pageProps, Component }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  )
+}
+
+MyApp.getInitialProps = async () => {
+  return {
+    pageProps: {
+      commrecePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  }
 }
