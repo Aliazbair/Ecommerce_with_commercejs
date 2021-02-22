79c4163f23611a42e42e2aaa692bb34235d2219c
ali <ali0995559052@gmail.com>
2021-02-22 01:41:48 -0800

    styels list products

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,60 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Link,
+  Slide,
+  Typography,
+} from '@material-ui/core'
+import { Alert } from '@material-ui/lab'
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import getCommerce from '../utils/commerce'
 export default function Home(props) {
+  const { products } = props
   return (
-    <Layout title="Home" commercePublicKey={props.commercePublicKey}>
-     <main>
-      {props.products.map((product) => (
-        <div key={product.id}>
-          <img src={product.media.source} alt={product.name} />
-          <p>{product.name}</p>
-          <p>{product.price.formatted_with_symbol}</p>
-        </div>
-      ))}
-
-     </main>
+    <Layout title='Home' commercePublicKey={props.commercePublicKey}>
+      {/* check the products */}
+      {products.length === 0 && <Alert>No Product found😥😥</Alert>}
+      <Grid container spacing={1}>
+        {products.map((product) => (
+          <Grid key={product.id} item md={3}>
+            <Slide direction='up' in={true}>
+              <Card>
+                <Link href={`/products/${product.permalink}`}>
+                  <CardActionArea>
+                    <CardMedia
+                      component='img'
+                      alt={product.name}
+                      image={product.media.source}
+                    />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant='body2'
+                        color='textPrimary'
+                        component='p'>
+                        {product.name}
+                      </Typography>
+                      <Box>
+                        <Typography
+                          variant='body1'
+                          color='textPrimary'
+                          component='p'>
+                          {product.price.formatted_with_symbol}
+                        </Typography>
+                      </Box>
+                    </CardContent>
+                  </CardActionArea>
+                </Link>
+              </Card>
+            </Slide>
+          </Grid>
+        ))}
+      </Grid>
     </Layout>
   )
 }
